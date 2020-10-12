3f68e996374cbda1b46c0e2892c75f8041fa58ff
ZhengyiWuu <70997287+ZhengyiWuu@users.noreply.github.com>
2020-10-12 18:09:44 +0800

    完善异常处理

--- calculator/src/util/MyException.java
+++ calculator/src/util/MyException.java
@@ -1,6 +1,13 @@
 package util;
 
 public class MyException extends Exception{
+    public MyException() {
+    }
+
+    public MyException(Throwable cause) {
+        super(cause);
+    }
+
     public MyException(String message) {
         super(message);
     }
